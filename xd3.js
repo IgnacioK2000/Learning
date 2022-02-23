27b723c107227329f88212633f5aef874b16fbb6
IgnacioK2000 <nachokrauze@gmail.com>
2022-02-23 14:12:00 -0300

    mas cuentas

---
+++ xd3.js
@@ -0,0 +1,8 @@
+function text3(){
+  var fer 
+  fer=200/3
+  fer=4+3
+  fer=7-1
+  fer=8+8 
+  console.log(fer)
+}text3()
---
+++ xd4.js
@@ -0,0 +1,8 @@
+function text4(){
+    var cami
+    cami=100+300
+    cami=cami*600
+    cami=cami/2
+    cami=cami-10
+       console.log(cami)
+}text4()
